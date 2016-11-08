5fecb6c7233779c7546f50a1b35a0f869419f360
Kansept <kansept@ya.ru>
2016-11-08 08:48:38 +0300

    Добавлена проверка на ошибки при импорте

--- commands/ImportController.php
+++ commands/ImportController.php
@@ -11,11 +11,15 @@ class ImportController extends Controller
      * This command echoes what you have entered as the message.
      * @param string $message the message to be echoed.
      */
-    public function actionOrlovAntennas()
+    public function actionOrlovAntennas($fileName = 'test.xlsx')
     {
-        $fileName = "/var/www/public/download/test.xlsx";
+        $filePath = "/var/www/public/download/" . $fileName;
+				if (!file_exists($filePath)) {
+						echo "File does not exist " . $filePath . "\n";
+						return 0;
+				}
         \PHPExcel_Settings::setCacheStorageMethod(\PHPExcel_CachedObjectStorageFactory::cache_in_memory_gzip);
-        $xls = \PHPExcel_IOFactory::load($fileName);
+        $xls = \PHPExcel_IOFactory::load($filePath);
         $xls->setActiveSheetIndex(0);
         $sheet = $xls->getActiveSheet();
 
@@ -34,9 +38,12 @@ class ImportController extends Controller
 								'antenna'      => (string) $sheet->getCell('P' . $row)->getValue(),
 								'polarization' => (string) $sheet->getCell('Q' . $row)->getValue(),
             ]);
-            $OrlovAntennasItem->insert();
+						if (!$OrlovAntennasItem->insert()) {
+								echo "row - " . $row . "\n";
+								print_r($OrlovAntennasItem->getErrors());
+						}
             unset($OrlovAntennasItem);
-        }
+				}
 
         echo "import rows - " . $sheet->getHighestRow() . "\n";
 
