82dcd3ac890497e281e3fbe7aa5f8359653846f8
Kansept <kansept@ya.ru>
2016-11-21 10:33:34 +0300

    Исправлена миграция таблицы customer

--- migrations/m161121_055729_create_customer_table.php
+++ migrations/m161121_055729_create_customer_table.php
@@ -14,7 +14,7 @@ class m161121_055729_create_customer_table extends Migration
     {
         $this->createTable('customer', [
             'id' => $this->primaryKey(),
-            'name' => $this->string(100),
+            'name' => $this->string(100)->notNull()->unique(),
         ]);
     }
 
