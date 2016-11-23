de1f31fad743b43259d558ced484ba4083ca4fbd
Kansept <kansept@ya.ru>
2016-11-23 17:02:48 +0300

    Изменена главная страница

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -60,7 +60,37 @@ class SiteController extends Controller
      */
     public function actionIndex()
     {
-        return $this->render('index');
+        $baseStations = \app\models\BaseStation::find()
+            ->select(['count(*) as count', 'mobile_operator.name'])
+            ->leftJoin('mobile_operator', '`base_station`.`mobile_operator_id` = `mobile_operator`.`id`')
+            ->groupBy('mobile_operator_id')
+            ->asArray()
+            ->all();
+
+        $statuses = \app\models\Project::find()
+            ->select(['count(*) as count', '`status`.`name`'])
+            ->leftJoin('status', '`status`.`id` = `project`.`status_id`')
+            ->groupBy('status_id')
+            ->asArray()
+            ->all();
+
+        $debts = \app\models\Project::find()
+            ->select(['SUM(cost) AS sum_cost', 'SUM(IFNULL(paid, 0)) AS sum_paid'])
+            ->asArray()
+            ->one();
+
+        $projects = \app\models\Project::find()
+            ->where(['status_id' => 1])
+            ->limit(10)
+            ->orderBy('end_date ASC')
+            ->all();
+
+        return $this->render('index', [
+            'baseStations' => $baseStations,
+            'statuses' => $statuses,
+            'debts' => $debts,
+            'projects' => $projects,
+        ]);
     }
 
     /**
--- views/site/index.php
+++ views/site/index.php
@@ -1,53 +1,117 @@
 <?php
 
+use yii\helpers\Html;
+use yii\helpers\Url;
+
 /* @var $this yii\web\View */
 
 $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
 
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
+  <div class="jumbotron">
+    <h3>Добро пожаловать в SanPasport Manager!</h3>
+  </div>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+  <div class="body-content">
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+    <div class="row">
+
+      <div class="col-lg-4">
+        <h4>Базовые станции</h4>
+        <table class="table">
+          <thead>
+            <tr>
+              <th>Оператор</th>
+              <th>Кол-во</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php foreach($baseStations as $baseStartion) { ?>
+            <tr>
+              <td><?= $baseStartion['name'] ?></td>
+              <td><?= $baseStartion['count'] ?></td>
+            </tr>
+            <?php } ?>
+          </tbody>
+        </table>
+      </div>
+
+      <div class="col-lg-4">
+        <h4>Проекты</h4>
+        <table class="table">
+          <thead>
+            <tr>
+              <th>Статус</th>
+              <th>Кол-во</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php foreach ($statuses as $status) { ?>
+            <tr>
+              <td><?= $status['name']?></td>
+              <td><?= $status['count']?></td>
+            </tr>
+            <?php } ?>
+          </tbody>
+        </table>
+      </div>
+
+      <div class="col-lg-4">
+        <h4>Оплата</h4>
+        <table class="table">
+          <thead>
+            <tr>
+              <th></th>
+              <th>руб.</th>
+            </tr>    
+          </thead>           
+          <tbody>
+            <tr>
+              <td>Всего сделанно на сумму:</td>
+              <td><?= Yii::$app->formatter->asInteger($debts['sum_cost']) ?></td>
+            </tr>
+            <tr>
+              <td>Всего выплаченно:</td>
+              <td><?= Yii::$app->formatter->asInteger($debts['sum_paid']) ?></td>
+            </tr>
+            <tr>
+              <td>Долг:</td>
+              <td><?= Yii::$app->formatter->asInteger($debts['sum_cost'] - $debts['sum_paid']) ?></td>
+            </tr>
+          </tbody>         
+        </table>
+      </div>
+
     </div>
 
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
-        </div>
-
+    <div class="row">
+      <div class="col-lg-12">
+        <h4>Проекты в работе</h4>
+        <?php 
+          $searchModel = new \app\models\ProjectSearch();
+          $dataProvider = $searchModel->search(['ProjectSearch' => ['status_id' => '1'] ]);
+        ?>
+        <?= \yii\grid\GridView::widget([
+            'dataProvider' => $dataProvider,
+            'columns' => [
+                ['class' => 'yii\grid\SerialColumn'],
+                'regionName',
+                'name',
+                'baseStationName',
+                'statusName',
+                'customerName',
+                [
+                  'content' => function ($model, $key, $index, $column) {
+                    return Html::a('Открыть', Url::toRoute(['project\view']));
+                  }
+                ]
+                //['class' => 'yii\grid\ActionColumn'],
+            ],
+        ]); ?>
+      </div>
     </div>
+
+  </div>
+
 </div>
